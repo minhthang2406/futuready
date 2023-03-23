b41e88620f40a5f45f39014f93474e80e9277ebe
VY <VY@Oprekin-PC>
2023-03-23 22:14:00 +0700

    Add new dashboard page

---
+++ src/main/java/futuready/pages/dashboard_page.java
@@ -0,0 +1,20 @@
+package futuready.pages;
+
+import org.openqa.selenium.WebDriver;
+import futuready.pages.*;
+import futuready.base.*;
+import org.openqa.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.edge.*;
+import org.openqa.selenium.firefox.*;
+import org.testng.*;
+
+public class dashboard_page {
+	public static WebDriver driver;
+//Locator
+	
+//Function 
+	public void setup (WebDriver driver) throws Exception {
+		this.driver = driver;
+	}
+}
